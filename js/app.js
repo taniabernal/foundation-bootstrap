17c039b6a1571cddaf40855226d3bf08ae69a049
Tania Bernal <tanibu18@gmail.com>
2017-09-20 16:18:27 -0500

    Menu offcanvas

--- js/app.js
+++ js/app.js
@@ -3,5 +3,14 @@ $(document).foundation()
 $("[data-toggle-dia]").click(function (ev){
   const panel = $(this).data("toggleDia")
   $("#lineup-tabs").foundation("selectTab", panel)
+})
 
+const $offCanvas = $("#offCanvas")
+const $sticky = $("#sticky")
+
+$offCanvas.find("li").click(function (ev) {
+  $offCanvas.foundation("close")
+  setTimeout(function(){
+    $sticky.css("left", "0px")
+  }, 300)
 })
